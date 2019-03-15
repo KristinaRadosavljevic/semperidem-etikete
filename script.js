adf3460506447e6d28e3fe5ebc07f107dcab9e17
Kiki <you@example.com>
2019-03-15 18:49:25 +0100

    Fixed button background color

--- script.js
+++ script.js
@@ -11,73 +11,73 @@ var karton = document.getElementById("kartonske_div");
 
 satenB.addEventListener("click", function satenF() {
     poli.style.display = "none";
-    poliB.style.backgroundColor = "transparent";
+    poliB.className = "inactive";
     samolep.style.display = "none";
-    samolepB.style.backgroundColor = "transparent";
+    samolepB.className = "inactive";
     karton.style.display = "none";
-    kartonB.style.backgroundColor = "transparent";
+    kartonB.className = "inactive";
     if (saten.style.display == "none"){
         saten.style.display = "block";
         saten.scrollIntoView(false);
-        satenB.style.backgroundColor = "#cc7a00";
+        satenB.className = "active";
     }
     else {
         saten.style.display = "none";
-        satenB.style.backgroundColor = "transparent";
+        satenB.className = "inactive";
     }
 });
 
 poliB.addEventListener("click", function poliF() {
     saten.style.display = "none";
-    satenB.style.backgroundColor = "transparent";
+    satenB.className = "inactive";
     samolep.style.display = "none";
-    samolepB.style.backgroundColor = "transparent";
+    samolepB.className = "inactive";
     karton.style.display = "none";
-    kartonB.style.backgroundColor = "transparent";
+    kartonB.className = "inactive";
     if (poli.style.display == "none"){
         poli.style.display = "block";
         poli.scrollIntoView(false);
-        poliB.style.backgroundColor = "#cc7a00";
+        poliB.className = "active";
     }
     else {
         poli.style.display = "none";
-        poliB.style.backgroundColor = "transparent";
+        poliB.className = "inactive";
     }
 });
 
 samolepB.addEventListener("click", function samolepF() {
     saten.style.display = "none";
-    satenB.style.backgroundColor = "transparent";
+    satenB.className = "inactive";
     poli.style.display = "none";
-    poliB.style.backgroundColor = "transparent";
+    poliB.className = "inactive";
     karton.style.display = "none";
-    kartonB.style.backgroundColor = "transparent";
+    kartonB.className = "inactive";
     if (samolep.style.display == "none"){
         samolep.style.display = "block";
         samolep.scrollIntoView(false);
-        samolepB.style.backgroundColor = "#cc7a00";
+        samolepB.className = "active";
     }
     else {
         samolep.style.display = "none";
-        samolepB.style.backgroundColor = "transparent";
+        samolepB.className = "inactive";
     }
 });
 
 kartonB.addEventListener("click", function kartonF() {
     poli.style.display = "none";
-    poliB.style.backgroundColor = "transparent";
+    poliB.className = "inactive";
     samolep.style.display = "none";
-    samolepB.style.backgroundColor = "transparent";
+    samolepB.className = "inactive";
     saten.style.display = "none";
-    satenB.style.backgroundColor = "transparent";
+    satenB.className = "inactive";
     if (karton.style.display == "none"){
         karton.style.display = "block";
         karton.scrollIntoView(false);
-        kartonB.style.backgroundColor = "#cc7a00";
+        kartonB.className = "active";
     }
     else {
         karton.style.display = "none";
-        kartonB.style.backgroundColor = "transparent";
+        kartonB.className = "inactive";
     }
 });
 
