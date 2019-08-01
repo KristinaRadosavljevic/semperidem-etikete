06855ba7b3f6b5fb5159e3f102b5cbccc7bded88
Kiki <you@example.com>
2019-08-01 19:39:47 +0200

    Intro changes + back to top button for mobile

--- public/script.js
+++ public/script.js
@@ -2,20 +2,16 @@
 var satenB = document.getElementById("usivne_saten");
 var poliB = document.getElementById("usivne_poli");
 var samolepB = document.getElementById("samolepljive");
-var kartonB = document.getElementById("kartonske");
 
 var saten = document.getElementById("usivne_saten_div");
 var poli = document.getElementById("usivne_poli_div");
 var samolep = document.getElementById("samolepljive_div");
-var karton = document.getElementById("kartonske_div");
 
 satenB.addEventListener("click", function satenF() {
     poli.style.display = "none";
     poliB.className = "inactive";
     samolep.style.display = "none";
     samolepB.className = "inactive";
-    karton.style.display = "none";
-    kartonB.className = "inactive";
     if (saten.style.display == "none" || saten.style.display == ""){
         saten.style.display = "block";
         saten.scrollIntoView(false);
@@ -32,8 +28,6 @@ poliB.addEventListener("click", function poliF() {
     satenB.className = "inactive";
     samolep.style.display = "none";
     samolepB.className = "inactive";
-    karton.style.display = "none";
-    kartonB.className = "inactive";
     if (poli.style.display == "none" || poli.style.display == ""){
         poli.style.display = "block";
         poli.scrollIntoView(false);
@@ -50,8 +44,6 @@ samolepB.addEventListener("click", function samolepF() {
     satenB.className = "inactive";
     poli.style.display = "none";
     poliB.className = "inactive";
-    karton.style.display = "none";
-    kartonB.className = "inactive";
     if (samolep.style.display == "none" || samolep.style.display == ""){
         samolep.style.display = "block";
         samolep.scrollIntoView(false);
@@ -60,24 +52,6 @@ samolepB.addEventListener("click", function samolepF() {
     else {
         samolep.style.display = "none";
         samolepB.className = "inactive";
-    }
-});
-
-kartonB.addEventListener("click", function kartonF() {
-    poli.style.display = "none";
-    poliB.className = "inactive";
-    samolep.style.display = "none";
-    samolepB.className = "inactive";
-    saten.style.display = "none";
-    satenB.className = "inactive";
-    if (karton.style.display == "none" || karton.style.display == ""){
-        karton.style.display = "block";
-        karton.scrollIntoView(false);
-        kartonB.className = "active";
-    }
-    else {
-        karton.style.display = "none";
-        kartonB.className = "inactive";
     }
 });
 
@@ -91,12 +65,10 @@ var galleryDiv = document.getElementsByClassName("gallery_view");
 var satenI = document.getElementsByClassName("saten_gallery");
 var poliI = document.getElementsByClassName("poli_gallery");
 var samolepI = document.getElementsByClassName("samolep_gallery");
-var kartonI = document.getElementsByClassName("karton_gallery");
 
 var satenG = [satenI[0].src, satenI[1].src, satenI[2].src];
 var poliG = [poliI[0].src, poliI[1].src, poliI[2].src];
 var samolepG = [samolepI[0].src, samolepI[1].src, samolepI[2].src];
-var kartonG = [kartonI[0].src, kartonI[1].src, kartonI[2].src];
 
 // Opening the gallery view on the picture that was clicked
 function openGallery(categoryIndex, pictureIndex, galleryArray){
@@ -125,9 +97,6 @@ poliI[2].addEventListener("click", function(){openGallery(1, 2, poliG)});
 samolepI[0].addEventListener("click", function(){openGallery(2, 0, samolepG)});
 samolepI[1].addEventListener("click", function(){openGallery(2, 1, samolepG)});
 samolepI[2].addEventListener("click", function(){openGallery(2, 2, samolepG)});
-kartonI[0].addEventListener("click", function(){openGallery(3, 0, kartonG)});
-kartonI[1].addEventListener("click", function(){openGallery(3, 1, kartonG)});
-kartonI[2].addEventListener("click", function(){openGallery(3, 2, kartonG)});
 
 // Closing the gallery
 close[0].addEventListener("click", function(){
@@ -138,9 +107,6 @@ close[1].addEventListener("click", function(){
 });
 close[2].addEventListener("click", function(){
     galleryDiv[2].style.display = "none";
-});
-close[3].addEventListener("click", function(){
-    galleryDiv[3].style.display = "none";
 });
 
 // Going through gallery
@@ -165,19 +131,9 @@ function goToNext(index, galleryArray){
 prev[0].addEventListener("click", function(){goToPrev(0, satenG)});
 prev[1].addEventListener("click", function(){goToPrev(1, poliG)});
 prev[2].addEventListener("click", function(){goToPrev(2, samolepG)});
-prev[3].addEventListener("click", function(){goToPrev(3, kartonG)});
 next[0].addEventListener("click", function(){goToNext(0, satenG)});
 next[1].addEventListener("click", function(){goToNext(1, poliG)});
 next[2].addEventListener("click", function(){goToNext(2, samolepG)});
-next[3].addEventListener("click", function(){goToNext(3, kartonG)});
-
-//Shrinking the textarea
-if(screen.width <= 800){
-    document.getElementById("message").setAttribute("cols", 30);
-}
-else{
-    document.getElementById("message").setAttribute("cols", 50);
-}
 
 //Form submit
 document.getElementById("submit").addEventListener("click", function(){
